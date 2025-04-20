d639f793d6cbf37479992fb88f91ef55d911d80b
Komiya <102408484+pHo9UBenaA@users.noreply.github.com>
2025-04-20 21:06:27 +0900

    v0.2.0 (#2)
    * feat: refactor focusSearchBox
    
    * refactor & add unit test
    
    * red: focusSearchBox
    
    * green: focusSearchBox
    
    * Bump version to v0.2.0
    
    * deno fmt

---
+++ src/constants/focusSearchBox.ts
@@ -0,0 +1,8 @@
+export const ElementIds = {
+  authorRepositoryIssues: "repository-input",
+  authorRepositoryPulls: "js-issues-search",
+  authorRepositoryProjects: "project-search-input",
+  authorRepositories: "your-repos-filter",
+  authorProjects: "project-search-input",
+  authorStars: "q",
+} as const;
